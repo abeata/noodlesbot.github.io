8dd472ed8c0766f8f077298a7c4225c2bbd97fcb
ab3ata <72804385+abeata@users.noreply.github.com>
2022-04-09 17:57:36 +0200

    Create info.js

---
+++ Commands/info.js
@@ -0,0 +1,10 @@
+const discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+    var Embed = new discord.MessageEmbed()
+    .setTitle("hello")
+}
+
+module.exports.help = {
+    name: "info"
+}
