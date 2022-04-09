8c112f2b544bc7e67ef9ffb2fc5c0f99e2f1b855
ab3ata <72804385+abeata@users.noreply.github.com>
2022-04-09 16:20:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const { Client, Intents } = require("discord.js")
-const client = new Client({ intents: [Intents.FLAGS.GUILDS]})
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]})
 
 client.once("ready", () => {
     console.log(`${client.user.username} is online.`)
     client.user.setActivity("Testing", {type: "PLAYING"})
 })
-client.login(process.env.TOKEN)
+
+client.on("messageCreate", message => {
+    if(message.author.bot) return;
+
+    var prefix = "!";
+    var messageArray = message.content.split(" ");
+    var command = messageArray[0];
+    if(command = prefix + "test") return message.channel.send("testing command is working.");
+})
+
+client.login(process.env.TOKEN);
