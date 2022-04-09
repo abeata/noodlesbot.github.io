7223fd802ba3ccbd493f2e5b1e2ee4c1c41383d5
ab3ata <72804385+abeata@users.noreply.github.com>
2022-04-09 18:18:06 +0200

    Update info.js

--- Commands/info.js
+++ Commands/info.js
@@ -2,8 +2,8 @@ const discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
     var Embed = new discord.MessageEmbed()
-    .setTitle("hello")
-    message.channel.send(Embed)
+        .setTitle("hello")
+    message.channel.send({ embed: [Embed]})
 }
 
 module.exports.help = {
