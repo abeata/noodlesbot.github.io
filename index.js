9b11f852e584b1012a7480bdc3b72672a4f26036
ab3ata <72804385+abeata@users.noreply.github.com>
2022-04-09 16:14:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const {Client,Intents} = require("discord.js")
+const { Client, Intents } = require("discord.js")
 const client = new Client({ intents: [Intents.FLAGS.GUILDS]})
 
 client.once("ready", () => {
-    console.log("ready")
+    console.log(`${client.user.username} is online.`)
+    client.user.setActivity("Testing", {type: "PLAYING"})
 })
 client.login(process.env.TOKEN)
